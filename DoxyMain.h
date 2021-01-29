b88701caf112252abccf3bc59963a6b7e9716de4
buck-yeh <buck.yeh@gmail.com>
2021-01-29 18:37:15 +0800

    Add examples to Doxygen doc in DoxyMain.h

---
+++ DoxyMain.h
@@ -0,0 +1,15 @@
+/*! \namespace bux
+ *  \brief THE common namespace of \c bux library
+ */
+
+/// \example bux/test/test_expand_env.cpp
+/// \example parsergen/ParserGen/main.cpp
+/// \example parsergen/ParserGen/GrammarStrip.cpp
+/// \example parsergen/ScannerGen/main.cpp
+/// \example parsergen/example/BisonGLR/main.cpp
+/// \example parsergen/example/CalcInt/main.cpp
+/// \example parsergen/example/MinLang/main.cpp
+/// \example JSON/src/JsonObjs.cpp
+/// \example JSON/src/ParseJSON.cpp
+/// \example JSON/test/test_json.cpp
+/// \example ParseSQL/src/formatsql.cpp
