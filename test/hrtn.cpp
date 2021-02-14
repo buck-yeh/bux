85bfac5d4793cc34b36a4d4bfc6c239e1447cfca
buck-yeh <buck.yeh@gmail.com>
2021-02-14 18:54:30 +0800

    Warning-free clang build
    1. ✍️ Subdir test/ is excluded from build by default. To include test/, configure cmake with -DBUILD_TEST=1
    2. ✍️ `bux::C_EzArgs::parse()` accepts `argc` parameter both in type **size_t** or **int**
    3. ✍️ `Intervals.h` rehauled to use C++20 concepts to smartly reduce lines and eliminate clang warnings altogether.
    4. ✍️ Eliminate clang warnings here & there.
    5. 🆕 test/hrtn to demangle external symbols freely.

---
+++ test/hrtn.cpp
@@ -0,0 +1,10 @@
+#include <bux/LogStream.h>  // bux::_HRTN()
+#include <iostream>         // std::cout
+
+int main(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; ++i)
+        std::cout <<argv[i] <<"\t-> " <<bux::_HRTN(argv[i]) <<'\n';
+
+    std::cout <<"Total " <<(argc - 1) <<" conversion(s)\n";
+}
