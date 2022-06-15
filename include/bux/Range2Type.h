1a230b5c4c83517f4ed685f1f0509c4c0f847e37
buck-yeh <buck.yeh@gmail.com>
2022-06-15 23:28:27 +0800

    [Change] Modernize `bux::fittestType()`

--- include/bux/Range2Type.h
+++ include/bux/Range2Type.h
@@ -1,162 +1,65 @@
 #pragma once
 
 #include "StrUtil.h"    // HRTN()
+#include <concepts>     // std::integral<>
 #include <limits>       // std::numeric_limits<>
 #include <cstdint>      // std::uint8_t, std::uint16_t, std::uint32_t
 
 namespace bux {
 
 //
-//      Types
-//
-template<class T, bool IS_SIGNED>
-struct C_FittestVeryLongType
-{
-    static std::string run(T lb, T ub);
-};
-
-template<class T>
-struct C_FittestVeryLongType<T,true>
-{
-    static std::string run(T lb, T ub)
-    {
-        if (std::numeric_limits<std::int8_t>::min() <= lb &&
-            ub <= std::numeric_limits<std::int8_t>::max())
-            return "int8_t";
-        if (std::numeric_limits<std::int16_t>::min() <= lb &&
-            ub <= std::numeric_limits<std::int16_t>::max())
-            return "int16_t";
-        if (std::numeric_limits<std::int32_t>::min() <= lb &&
-            ub <= std::numeric_limits<std::int32_t>::max())
-            return "int32_t";
-        return HRTN(T);
-    }
-};
-
-template<class T>
-struct C_FittestVeryLongType<T,false>
-{
-    static std::string run(T, T ub)
-    {
-        if (ub <= (T)std::numeric_limits<std::uint8_t>::max())
-            return "uint8_t";
-        if (ub <= (T)std::numeric_limits<std::uint16_t>::max())
-            return "uint16_t";
-        if (ub <= (T)std::numeric_limits<std::uint32_t>::max())
-            return "uint32_t";
-        return HRTN(T);
-    }
-};
-
-template<class T, size_t N>
-struct C_FittestTypeN
-{
-    static std::string run(T lb, T ub)
-    {
-        // (T(-1) < 0) as a substitute of numeric_limits<T>::is_signed
-        return C_FittestVeryLongType<T,T(-1)<0>::run(lb, ub);
-    }
-};
-
-template<class T>
-struct C_FittestTypeN<T,1>
-{
-    static std::string run(T, T)
-    {
-        return HRTN(T);
-    }
-};
-
-template<class T, bool IS_SIGNED>
-struct C_FittestType2S
-{
-    static std::string run(T lb, T ub);
-};
-
-template<class T>
-struct C_FittestType2S<T,true>
-{
-    static std::string run(T lb, T ub)
-    {
-        if (std::numeric_limits<std::int8_t>::min() <= lb &&
-            ub <= std::numeric_limits<std::int8_t>::max())
-            return "int8_t";
-        return HRTN(T);
-    }
-};
-
-template<class T>
-struct C_FittestType2S<T,false>
-{
-    static std::string run(T, T ub)
-    {
-        if (ub <= (T)std::numeric_limits<std::uint8_t>::max())
-            return "uint8_t";
-        return HRTN(T);
-    }
-};
-
-template<class T>
-struct C_FittestTypeN<T,2>
-{
-    static std::string run(T lb, T ub)
-    {
-        // (T(-1) < 0) as a substitute of numeric_limits<T>::is_signed
-        return C_FittestType2S<T,T(-1)<0>::run(lb, ub);
-    }
-};
-
-template<class T, bool IS_SIGNED>
-struct C_FittestType4S
-{
-    static std::string run(T lb, T ub);
-};
-
-template<class T>
-struct C_FittestType4S<T,true>
-{
-    static std::string run(T lb, T ub)
-    {
-        if (std::numeric_limits<std::int8_t>::min() <= lb &&
-            ub <= std::numeric_limits<std::int8_t>::max())
-            return "int8_t";
-        if (std::numeric_limits<std::int16_t>::min() <= lb &&
-            ub <= std::numeric_limits<std::int16_t>::max())
-            return "int16_t";
-        return HRTN(T);
-    }
-};
-
-template<class T>
-struct C_FittestType4S<T,false>
-{
-    static std::string run(T, T ub)
-    {
-        if (ub <= (T)std::numeric_limits<std::uint8_t>::max())
-            return "uint8_t";
-        if (ub <= (T)std::numeric_limits<std::uint16_t>::max())
-            return "uint16_t";
-        return HRTN(T);
-    }
-};
-
-template<class T>
-struct C_FittestTypeN<T,4>
-{
-    static std::string run(T lb, T ub)
-    {
-        // (T(-1) < 0) as a substitute of numeric_limits<T>::is_signed
-        return C_FittestType4S<T,T(-1)<0>::run(lb, ub);
-    }
-};
-
-//
 //      Fnction Templates
 //
-template<class T>
-inline std::string fittestType(T lb, T ub)
+template<std::integral T>
+std::string fittestType(T lb, T ub)
+/* \param lb Lower bound of value range
+   \param ub Upper bound of value range
+   \pre <tt> lb <= ub </tt>
+   \return Name of the fittest type
+*/
 {
-    return C_FittestTypeN<T,sizeof(T)>::run(lb, ub);
+    if constexpr (std::numeric_limits<T>::is_signed)
+    {
+        if (std::numeric_limits<std::int8_t>::min() <= lb &&
+            ub <= std::numeric_limits<std::int8_t>::max())
+            return "int8_t";
+
+        if constexpr (sizeof(T) >= sizeof(std::int16_t))
+        {
+            if (std::numeric_limits<std::int16_t>::min() <= lb &&
+                ub <= std::numeric_limits<std::int16_t>::max())
+                return "int16_t";
+        }
+
+        if constexpr (sizeof(T) >= sizeof(std::int32_t))
+        {
+            if (std::numeric_limits<std::int32_t>::min() <= lb &&
+                ub <= std::numeric_limits<std::int32_t>::max())
+                return "int32_t";
+        }
+
+        // There could be more ..
+    }
+    else
+    {
+        if (ub <= (T)std::numeric_limits<std::uint8_t>::max())
+            return "uint8_t";
+
+        if constexpr (sizeof(T) >= sizeof(std::uint16_t))
+        {
+            if (ub <= (T)std::numeric_limits<std::uint16_t>::max())
+                return "uint16_t";
+        }
+
+        if constexpr (sizeof(T) >= sizeof(std::uint32_t))
+        {
+            if (ub <= (T)std::numeric_limits<std::uint32_t>::max())
+                return "uint32_t";
+        }
+
+        // There could be more ..
+    }
+    return HRTN(T);
 }
 
 } //namespace bux
