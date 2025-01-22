6599a81aebb76b2d0d7dcb685c5a19cf5ad1f354
buck.yeh <buck.yeh@iqt.ai>
2025-01-22 17:20:56 +0800

    [Bug] Multiple definition of `bux::local_zone()`

--- include/bux/XPlatform.h
+++ include/bux/XPlatform.h
@@ -16,7 +16,7 @@ namespace bux {
 
 #ifndef _LIBCPP_HAS_NO_TIME_ZONE_DATABASE
 using T_LocalZone = const std::chrono::time_zone *;
-T_LocalZone local_zone() { return std::chrono::get_tzdb().current_zone(); }
+inline T_LocalZone local_zone() { return std::chrono::get_tzdb().current_zone(); }
 #define LOCALZONE_IS_TIMEZONE (1)
 #else
 using T_LocalZone = bool;
